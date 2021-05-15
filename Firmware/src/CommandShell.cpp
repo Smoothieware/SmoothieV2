49ccd83f3037dd329955421506ec4f3f627d2785
Jim Morris <morris@wolfman.com>
2021-05-15 23:28:19 +0100

    remove printout of uid

--- Firmware/src/CommandShell.cpp
+++ Firmware/src/CommandShell.cpp
@@ -1229,7 +1229,6 @@ bool CommandShell::jog_cmd(std::string& params, OutputStream& os)
 }
 
 std::string get_mcu();
-extern "C" uint64_t get_uid();
 bool CommandShell::version_cmd(std::string& params, OutputStream& os)
 {
     HELP("version - print version");
@@ -1239,7 +1238,7 @@ bool CommandShell::version_cmd(std::string& params, OutputStream& os)
     os.printf("%s on %s\n", get_mcu().c_str(), BUILD_TARGET);
     os.printf("Build version: %s, Build date: %s, System Clock: %ldMHz\r\n", vers.get_build(), vers.get_build_date(), SystemCoreClock / 1000000);
     os.printf("%d axis\n", MAX_ROBOT_ACTUATORS);
-    os.printf("uid %016llX\n", get_uid());
+
     os.set_no_response();
 
     return true;
