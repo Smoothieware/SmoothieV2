72f6f7c7678de37f06ffd6a11d368947e0cbf481
Jim Morris <morris@wolfman.com>
2023-04-24 18:11:01 +0100

    initial commit for ST7920 driver (needs optimization) Add write_byte to SPI class

---
+++ Firmware/src/modules/utils/display/st7920/ST7920.h
@@ -0,0 +1,44 @@
+#pragma once
+
+/**
+ * Based loosely on st7920.h from http://mbed.org/users/Bas/code/ST7920 and parts of the Arduino U8glib library.
+ * Written by Jim Morris originally for Smoothieware V1
+ */
+
+#include "Module.h"
+
+class Pin;
+class SPI;
+class ConfigReader;
+
+class ST7920  : public Module
+{
+public:
+    ST7920 ();
+    virtual ~ST7920();
+
+    static bool create(ConfigReader& cr);
+    bool configure(ConfigReader& cr);
+
+    void initDisplay(void);
+    void clearScreen(void);
+    void displayString(int row, int column, const char *ptr, int length);
+    void refresh();
+
+    void fillGDRAM(const uint8_t *bitmap);
+
+    // copy the bits in g, of X line size pixels, to x, y in frame buffer
+    void renderGlyph(int x, int y, const uint8_t *g, int pixelWidth, int pixelHeight);
+
+private:
+    void renderChar(uint8_t *fb, char c, int ox, int oy);
+    void displayChar(int row, int column, char inpChr);
+
+    int spi_channel;
+    Pin *cs{nullptr};
+    SPI *spi{nullptr};
+    uint8_t *fb{nullptr};
+    bool inited{false};
+    bool dirty{false};
+};
+
